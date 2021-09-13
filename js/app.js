dde43d1e7ab306274ef7cec02a4a0b6f286655ba
salmanahmed643 <647salman@gmail.com>
2021-09-14 00:00:10 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -6,26 +6,32 @@ const loadProducts = () => {
 };
 loadProducts();
 
+
 // show all product in UI 
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
+
   for (const product of allProducts) {
     const image = product.images;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${product.image}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h5>Rating: <i class="fas fa-star"></i> ${product.rating.rate}</h5>
+      <h5>${product.rating.count} Total Rating</h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" onclick="loadDetail('${product.id}')" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -35,24 +41,28 @@ const addToCart = (id, price) => {
   document.getElementById("total-Products").innerText = count;
 };
 
+
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
+
 
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
+
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText =  value.toFixed(2);
 };
+
 
 // update delivery charge and total Tax
 const updateTaxAndCharge = () => {
@@ -71,10 +81,40 @@ const updateTaxAndCharge = () => {
   }
 };
 
+
 //grandTotal update function
 const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
+};
+
+
+
+
+// loadDetail
+const loadDetail = productId => {
+  const url = `https://fakestoreapi.com/products/${productId}`
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayDetails(data))
+}
+
+
+// displayDetails
+const displayDetails = product => {
+  console.log(product)
+  const singleProduct = document.getElementById('product_details');
+  singleProduct.textContent = '';
+  const div = document.createElement('div')
+  div.innerHTML = `
+        <div class="card-header"> <h3> Price: ${product.price} </h3> </div>
+        <div class="card-body">
+          <div class="card-title"> <h3> Name </h3> ${product.title} </div>
+          <p class="card-text"> <h4> Detail </h4> ${product.description} </p>
+        </div>
+  `
+  singleProduct.appendChild(div);
+  window.scroll(0, 40)
 };
